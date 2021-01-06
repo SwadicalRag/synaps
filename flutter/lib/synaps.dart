7090a75e01f8782e9f5afda665c21bb11a14c1f6
swadical <swadical@lab.glua.team>
2021-01-06 21:32:39 +1030

    Add `synaps_flutter` library

--- flutter/lib/synaps.dart
+++
@@ -1,7 +0,0 @@
-library synaps;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
---
+++ flutter/lib/synaps_flutter.dart
@@ -0,0 +1,57 @@
+library synaps;
+
+export 'package:synaps/synaps.dart';
+
+import 'package:flutter/widgets.dart';
+import 'package:synaps/synaps.dart';
+
+class Rx extends StatefulWidget {
+  final Widget Function() _buildFunc;
+
+  Rx(this._buildFunc);
+
+  RxState createState() => RxState();
+}
+
+class RxState extends State<Rx> {
+  MonitorState _monitorState;
+
+  @override
+  dispose() {
+    if(_monitorState != null) {
+      _monitorState.dispose();
+      _monitorState = null;
+    }
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    if(_monitorState != null) {
+      _monitorState.dispose();
+      _monitorState = null;
+    }
+
+    Widget out;
+
+    _monitorState = SynapsMasterController.monitor(
+      monitor: () {
+        out = widget._buildFunc();
+      },
+      onUpdate: () {
+        // Run setState to mark this widget for a re-build
+        if (mounted) {
+          this.setState(() {});
+        }
+      }
+    );
+
+    if(!_monitorState.hasCapturedSymbols) {
+      throw """
+      [synaps_flutter] No observable variables were detected directly inside this widget.
+      Are you sure you are using Rx() correctly?
+      """;
+    }
+
+    return out;
+  }
+}
